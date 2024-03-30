74f6e55e005425e751e61c36f2b1bdccf9455393
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2024-03-30 11:53:20 +0530

    robodshop shell

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,5 @@
 dnf install nginx -y
 cp roboshop-nginx.conf etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
